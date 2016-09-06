1020044f3a203ccc2d39f6b26b3fd6184e1e9109
Alexander I.Grafov <grafov@gmail.com>
2016-09-06 20:52:47 +0300

    Add docs.

---
+++ doc.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2016, Alexander I.Grafov aka Axel
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// * Neither the name of kvlog nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// ॐ तारे तुत्तारे तुरे स्व
+
+/*
+
+Package kiwi is a library with an odd logic that log your application' data in its own strange way.
+
+WIP. API and features is subject of changes. Don't use it yet!
+
+Features briefly:
+
+ * structured logging in `logfmt` for high readability by humans
+
+ * optional JSON format that liked by machines
+
+ * dynamically selectable outputs (changing log verbosity on the fly)
+
+ * there are not nailed levels, not hardcoded fields in the format
+
+ * can keep context of application
+
+Kiwi logger has two primary design goals:
+
+ 1. Convenient structured logging syntax: logfmt as default format, method chaining.
+
+ 2. Separation of logging flow from control flow: you log everything without conditions but output filtering really select what and where will be saved.
+*/
+package kiwi
